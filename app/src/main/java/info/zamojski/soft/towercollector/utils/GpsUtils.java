2fdf3f1a2837ec4f975318f61524412987bb997d
Adam <zamojski@users.noreply.github.com>
2019-05-06 20:47:20 +0200

    Fix crash when checking for GPS availability denied on API lower than 21.

--- app/src/main/java/info/zamojski/soft/towercollector/utils/GpsUtils.java
+++ app/src/main/java/info/zamojski/soft/towercollector/utils/GpsUtils.java
@@ -4,14 +4,21 @@
 
 package info.zamojski.soft.towercollector.utils;
 
+import android.Manifest;
 import android.content.Context;
 import android.location.LocationManager;
+import android.support.v4.content.PermissionChecker;
 
 public class GpsUtils {
 
     public static boolean isGpsEnabled(Context context) {
-        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (PermissionChecker.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PermissionChecker.PERMISSION_GRANTED) {
+            LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+            return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        }
+        // to cover the case when permission denied on API lower than 21
+        return false;
     }
 
     public static boolean isGpsAvailable(Context context) {
