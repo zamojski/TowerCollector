eb2035a3c6390f19e9227b98e3503c633e1bba47
Adam <zamojski@users.noreply.github.com>
2019-06-13 16:33:28 +0200

    Fix first measurement additional sorting.

--- app/src/main/java/info/zamojski/soft/towercollector/dao/MeasurementsDatabase.java
+++ app/src/main/java/info/zamojski/soft/towercollector/dao/MeasurementsDatabase.java
@@ -183,15 +183,16 @@ public class MeasurementsDatabase {
 
     public Measurement getFirstMeasurement() {
         Measurement firstMeasurement = null;
-        List<Measurement> measurements = getMeasurements(CellSignalsTable.TABLE_NAME + "." + CellSignalsTable.COLUMN_MEASUREMENT_ID + " = " + "(SELECT tm." + MeasurementsTable.COLUMN_ROW_ID + " FROM " + NotUploadedMeasurementsView.VIEW_NAME + " tm ORDER BY tm." + MeasurementsTable.COLUMN_MEASURED_AT + " ASC, tm." + MeasurementsTable.COLUMN_ROW_ID + " DESC LIMIT 0,1)",
+        List<Measurement> measurements = getMeasurements(CellSignalsTable.TABLE_NAME + "." + CellSignalsTable.COLUMN_MEASUREMENT_ID + " = " + "(SELECT tm." + MeasurementsTable.COLUMN_ROW_ID + " FROM " + NotUploadedMeasurementsView.VIEW_NAME + " tm ORDER BY tm." + MeasurementsTable.COLUMN_MEASURED_AT + " ASC, tm." + MeasurementsTable.COLUMN_ROW_ID + " ASC LIMIT 0,1)",
                 null,
                 null, null,
                 NotUploadedMeasurementsView.VIEW_NAME + "." + MeasurementsTable.COLUMN_MEASURED_AT + " ASC, " // from view because not for upload
                         + CellSignalsTable.TABLE_NAME + "." + CellSignalsTable.COLUMN_NEIGHBORING + " ASC, "
                         + CellSignalsTable.TABLE_NAME + "." + CellSignalsTable.COLUMN_ROW_ID + " ASC",
                 null, false);
-        if (!measurements.isEmpty())
+        if (!measurements.isEmpty()) {
             firstMeasurement = measurements.get(0);
+        }
         Timber.d("getFirstMeasurement(): %s", firstMeasurement);
         return firstMeasurement;
     }
