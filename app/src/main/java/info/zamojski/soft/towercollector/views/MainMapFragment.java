c53278d7c5c14c418a68fc4485d196bc0fa8f722
Adam <zamojski@users.noreply.github.com>
2020-05-30 15:31:42 +0200

    Temporarily display last 1000 measurements with colorful icons

--- app/src/main/java/info/zamojski/soft/towercollector/views/MainMapFragment.java
+++ app/src/main/java/info/zamojski/soft/towercollector/views/MainMapFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.DrawableRes;
 import androidx.preference.PreferenceManager;
 
 import org.greenrobot.eventbus.Subscribe;
@@ -24,16 +25,22 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import info.zamojski.soft.towercollector.MyApplication;
 import info.zamojski.soft.towercollector.R;
+import info.zamojski.soft.towercollector.dao.MeasurementsDatabase;
 import info.zamojski.soft.towercollector.events.MeasurementSavedEvent;
 import info.zamojski.soft.towercollector.events.PrintMainWindowEvent;
+import info.zamojski.soft.towercollector.model.Cell;
+import info.zamojski.soft.towercollector.model.Measurement;
 import info.zamojski.soft.towercollector.utils.FileUtils;
+import info.zamojski.soft.towercollector.utils.NetworkTypeUtils;
 
 public class MainMapFragment extends MainFragmentBase {
 
@@ -92,8 +99,39 @@ public class MainMapFragment extends MainFragmentBase {
                 return false;
             }
         }));
-        GeoPoint startPoint = new GeoPoint(52.069167, 19.480556);
-        mapController.setCenter(startPoint);
+
+        printLastMeasurements(mapController);
+    }
+
+    private void printLastMeasurements(IMapController mapController) {
+        Measurement lastMeasurement = MeasurementsDatabase.getInstance(getContext()).getLastMeasurement();
+        if (lastMeasurement != null) {
+            GeoPoint startPoint = new GeoPoint(lastMeasurement.getLatitude(), lastMeasurement.getLongitude());
+            mapController.setCenter(startPoint);
+
+            // TODO: temporarily load 1000 measurements instead of these visible
+            List<Measurement> measurements = MeasurementsDatabase.getInstance(getContext()).getMeasurementsPart(0, 1000);
+
+            // TODO: don't print on main thread
+            // create markers
+            List<Marker> items = new ArrayList<>();
+            for (Measurement m : measurements) {
+                Marker item = new Marker(mainMapView);
+                List<Cell> cells = m.getMainCells();
+                @DrawableRes int iconId;
+                if (cells.size() == 1) {
+                    iconId = NetworkTypeUtils.getNetworkGroupIcon(cells.get(0).getNetworkType());
+                } else {
+                    iconId = NetworkTypeUtils.getNetworkGroupIcon(cells.get(0).getNetworkType(), cells.get(1).getNetworkType());
+                }
+                item.setIcon(getResources().getDrawable(iconId));
+                item.setPosition(new GeoPoint(m.getLatitude(), m.getLongitude()));
+                items.add(item);
+            }
+
+            // add overlay
+            mainMapView.getOverlays().addAll(items);
+        }
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
